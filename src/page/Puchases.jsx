2c54822e12f44f45d46d05b9f8b1ca7e47716f91
fdlaurentis <fdlaurentis@gmail.com>
2022-08-11 19:21:33 -0500

    update card puchases

--- src/page/Puchases.jsx
+++ src/page/Puchases.jsx
@@ -2,30 +2,46 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPuchasesThunk } from '../store/slice/puchases.slice';
+import { Card } from 'react-bootstrap'
 
 const Puchases = () => {
 
-    const puchases=useSelector(state=>state.puchases)
+    const puchases = useSelector(state => state.puchases)
     const dispatch = useDispatch()
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getPuchasesThunk())
-    },[])
+    }, [])
+
+    // const formatDate=date=>{
+    //     const dateFormat=date.toStringDate()
+    //     console.log(dateFormat)
+    // }
 
     return (
         <div className='container my-5'>
-            <h1>Puchases</h1>            
+            <h1>Puchases</h1>
+            
                 {
-                    puchases.map(puchase=>(
-                        <ul key={puchase.id}>
-                            {puchase.cart.products.map(puchaseCart=>(
+                    puchases.map(puchase => (
+                        <Card key={puchase.id}>
+                        <ul >
+                            <Card.Header>{puchase.createdAt}</Card.Header>
+                            {puchase.cart.products.map(puchaseCart => (
                                 <li key={puchaseCart.id}>
-                                    {puchaseCart.title}
+                                    <Card.Body>
+                                        <Card.Title>{puchaseCart.title}</Card.Title>
+                                    </Card.Body>
                                 </li>
                             ))}
                         </ul>
+                        </Card>
                     ))
-                }            
+                }
+
+
+            
+
         </div>
     );
 };
